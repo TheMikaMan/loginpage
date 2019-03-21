4ac0834e7026715b3b076b1823a8c323d45789ca
TheMikaMan <47526162+TheMikaMan@users.noreply.github.com>
2019-03-21 11:26:02 +0100

    nieuw

--- index.php
+++ index.php
@@ -4,11 +4,9 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div class="form-wrapper">
+
     <div>voorkant></div>
-	 <div class="reminder">
-    <p><a href="logout.php">Log out</a></p>
-  </div>
-</div>
+
+
 </body>
 </html>
